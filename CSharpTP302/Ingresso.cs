3a734267314b58a01a70be8c428c360d67412011
Marcelo Sampaio <marcelosampaiio1965@gmail.com>
2026-09-08 16:00:08 -0300

    feat: Exercicio 2

---
+++ CSharpTP302/Ingresso.cs
@@ -0,0 +1,11 @@
+public class Ingresso
+{
+    // Nome do show ao qual o ingresso pertence.
+    public string nomeDoShow;
+
+    // Preço de cada ingresso.
+    public double preco;
+
+    // Quantidade de ingressos disponíveis para venda.
+    public int quantidadeDisponivel;
+}
---
+++ CSharpTP302/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Criação de um objeto da classe Ingresso.
+        Ingresso ingresso = new Ingresso();
+
+        // Atribuição de valores aos atributos do objeto.
+        ingresso.nomeDoShow = "Festival de Música";
+        ingresso.preco = 150.00;
+        ingresso.quantidadeDisponivel = 500;
+
+        // Exibição dos dados do ingresso
+        Console.WriteLine("Nome do show: " + ingresso.nomeDoShow);
+        Console.WriteLine("Preço: R$ " + ingresso.preco);
+        Console.WriteLine("Quantidade disponível: " + ingresso.quantidadeDisponivel);
+    }
+}
