5f6cd1044b665569ea86e670dc5c74cc08c38c83
Marcelo Sampaio <marcelosampaiio1965@gmail.com>
2026-09-08 21:27:31 -0300

    feat: Exercicio 6

---
+++ CSharpTP306/Program.cs
@@ -0,0 +1,9 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Ingresso ingresso = new Ingresso("Festival de Música", 180.00, 450);
+
+        ingresso.ExibirInformacoes();
+    }
+}
