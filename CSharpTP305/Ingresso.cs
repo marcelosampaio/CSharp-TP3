8ea51eca5e6ceb3a952e7a23deb5d1c5b2038deb
Marcelo Sampaio <marcelosampaiio1965@gmail.com>
2026-09-08 21:06:58 -0300

    feat: Exercicio 5

---
+++ CSharpTP305/Ingresso.cs
@@ -0,0 +1,53 @@
+public class Ingresso
+{
+    public string nomeDoShow;
+    public double preco;
+    public int quantidadeDisponivel;
+
+    public void AlterarPreco(double novoPreco)
+    {
+        preco = novoPreco;
+    }
+
+    public void AlterarQuantidade(int novaQuantidade)
+    {
+        quantidadeDisponivel = novaQuantidade;
+    }
+
+    public void ExibirInformacoes()
+    {
+        Console.WriteLine("Nome do show: " + nomeDoShow);
+        Console.WriteLine("Preço: R$ " + preco);
+        Console.WriteLine("Quantidade disponível: " + quantidadeDisponivel);
+    }
+    
+    public string GetNomeDoShow()
+    {
+        return nomeDoShow;
+    }
+
+    public double GetPreco()
+    {
+        return preco;
+    }
+
+    public int GetQuantidadeDisponivel()
+    {
+        return quantidadeDisponivel;
+    }
+
+    public void SetNomeDoShow(string novoNome)
+    {
+        nomeDoShow = novoNome;
+    }
+
+    public void SetPreco(double novoPreco)
+    {
+        preco = novoPreco;
+    }
+
+    public void SetQuantidadeDisponivel(int novaQtd)
+    {
+        quantidadeDisponivel = novaQtd;
+    }
+}
---
+++ CSharpTP305/Program.cs
@@ -0,0 +1,15 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Ingresso ingresso = new Ingresso();
+
+        ingresso.SetNomeDoShow("Festival de Música");
+        ingresso.SetPreco(180.00);
+        ingresso.SetQuantidadeDisponivel(450);
+
+        Console.WriteLine("Nome do show: " + ingresso.GetNomeDoShow());
+        Console.WriteLine("Preço: R$ " + ingresso.GetPreco());
+        Console.WriteLine("Quantidade disponível: " + ingresso.GetQuantidadeDisponivel());
+    }
+}
