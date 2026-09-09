08805b07629ac5475f0706d36b5d0d007805b074
Marcelo Sampaio <marcelosampaiio1965@gmail.com>
2026-09-08 21:42:13 -0300

    feat: Exercicio 7

---
+++ CSharpTP307/Matricula.cs
@@ -0,0 +1,8 @@
+public class Matricula
+{
+    public string NomeDoAluno;
+    public string Curso;
+    public int NumeroMatricula;
+    public string Situacao;
+    public string DataInicial;
+}
---
+++ CSharpTP307/Program.cs
@@ -0,0 +1,19 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Matricula matricula = new Matricula();
+
+        matricula.NomeDoAluno = "Marcelo Sampaio";
+        matricula.Curso = "Análise e Desenvolvimento de Sistemas";
+        matricula.NumeroMatricula = 12345;
+        matricula.Situacao = "Ativa";
+        matricula.DataInicial = "01/08/2026";
+
+        Console.WriteLine("Nome do aluno: " + matricula.NomeDoAluno);
+        Console.WriteLine("Curso: " + matricula.Curso);
+        Console.WriteLine("Número da matrícula: " + matricula.NumeroMatricula);
+        Console.WriteLine("Situação: " + matricula.Situacao);
+        Console.WriteLine("Data inicial: " + matricula.DataInicial);
+    }
+}
