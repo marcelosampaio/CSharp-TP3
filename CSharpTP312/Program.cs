438569c9949c3e4d39a6aa29a5b2a88132b3915a
Marcelo Sampaio <marcelosampaiio1965@gmail.com>
2026-09-09 08:46:54 -0300

    feat: Exercicio 12

---
+++ CSharpTP312/Program.cs
@@ -0,0 +1,13 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Circulo circulo = new Circulo();
+        circulo.Raio = 3;
+        Console.WriteLine("Área do círculo: " + circulo.CalcularArea());
+
+        Esfera esfera = new Esfera();
+        esfera.Raio = 5;
+        Console.WriteLine("Volume da esfera: " + esfera.CalcularVolume());
+    }
+}
